86d2850a0474c2367351924086bc3afcb11e9328
UJJWAL <ujjwaltambe@gmail.com>
2021-03-02 10:50:45 +0530

    Entry code

---
+++ Entry.py
@@ -0,0 +1,36 @@
+# This widget is used to get the text data from user via GUI
+# A simple box is provided on the interface where the user can input text.
+# This text can then be retrieved and used by us through the use special functions.
+
+# syntax: Entry = Entry(master, option.........)
+
+# program takes a input data from entry widget and clicked button to take action specified.
+# In this after clicking button we are showing the same data in labels
+
+from tkinter import *
+
+def retrieve_data():
+    var.set(entry_1.get())
+
+root = Tk()
+root.geometry("480x340")
+
+frame = Frame(root)
+frame.pack()
+
+# creating entry to take input
+entry_1 = Entry(frame, width = 15)
+entry_1.insert(50,'Give Input')  # insert is use to store provided string at 0 index
+entry_1.pack(padx = 10 , pady =10)
+
+var = StringVar()
+var.set("   ")
+
+label = Label(frame, textvariable= var, width = 15, fg = "white", bg ='black')
+label.pack()  # without pack function widget won't get displayed
+
+# creating button to perform some action
+button = Button(frame, text = "Button", command = retrieve_data , fg = 'red' )
+button.pack()
+
+root.mainloop()
--- button.py
+++ button.py
@@ -1,16 +1,39 @@
+# this example explains usage of button, lable
+# Label is a widget which is use to show some text data
+# button is an input to the GUI which will perform defined action.
+
 from tkinter import *
 
 
-def set():
-    print("Hello World")
+def set_1():
+    print("You clicked the button")
+    var1.set(" clicked")
 
+def set():
+    var.set("Good-Bye Cruel World")  # can use input() to take directly data from user
 
 root = Tk()
 root.geometry("200x150")
 
+button = Button(root, text="Button1", command=set_1) # dont use parathesis with function name
+button.pack()
+
 frame = Frame(root)
 frame.pack()
-button = Button(frame, text="Button1", command=set)
-button.pack()
+
+var = StringVar()
+var.set("Hello World")
+
+var1 = StringVar()
+var1.set("customized")
+
+label = Label(frame, textvariable=var)
+label.pack()
+
+label = Label(frame, textvariable=var1)
+label.pack()
+
+button_1 = Button(frame, text="set", command=set)
+button_1.pack()
 
 root.mainloop()
